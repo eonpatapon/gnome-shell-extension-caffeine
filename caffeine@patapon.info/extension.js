608b8d1d8c0fdc0d82bb7ed79e06afab5065b81f
Jean-Philippe Braun <eon@patapon.info>
2014-03-03 09:44:59 +0100

    Better notification texts
    Closes #22

--- caffeine@patapon.info/extension.js
+++ caffeine@patapon.info/extension.js
@@ -225,7 +225,7 @@ const Caffeine = new Lang.Class({
                     this._state = true;
                     this._icon.icon_name = EnabledIcon;
                     if (this._settings.get_boolean(SHOW_NOTIFICATIONS_KEY))
-                        Main.notify(_("Caffeine enabled"));
+                        Main.notify(_("Auto suspend and screensaver disabled"));
                 }
             }
         }));
@@ -244,7 +244,7 @@ const Caffeine = new Lang.Class({
                 this._state = false;
                 this._icon.icon_name = DisabledIcon;
                 if(this._settings.get_boolean(SHOW_NOTIFICATIONS_KEY))
-                    Main.notify(_("Caffeine disabled"));
+                    Main.notify(_("Auto suspend and screensaver enabled"));
             }
         }
     },
