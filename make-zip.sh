584a877aaafb55ae33463f18aeb81aa6295d5edb
Jean-Philippe Braun <eon@patapon.info>
2021-09-27 08:35:59 +0200

    Exclude po files and xml schema from zip

--- make-zip.sh
+++ make-zip.sh
@@ -2,4 +2,4 @@
 
 rm -f caffeine@patapon.info.zip
 glib-compile-schemas --strict --targetdir=caffeine@patapon.info/schemas/ caffeine@patapon.info/schemas
-cd caffeine@patapon.info && zip -r ../caffeine@patapon.info.zip *
+cd caffeine@patapon.info && zip -r ../caffeine@patapon.info.zip ./* --exclude \*.po --exclude \*.gschema.xml
