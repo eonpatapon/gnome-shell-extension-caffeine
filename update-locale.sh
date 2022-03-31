885b582b631ec1e5b9717536667efb38211232fc
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-03-31 20:10:28 +0100

    Updated locale updater to pass `shellcheck`

--- update-locale.sh
+++ update-locale.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
-cd caffeine@patapon.info
+cd caffeine@patapon.info || exit 1
 
-pot=gnome-shell-extension-caffeine.pot
+pot="gnome-shell-extension-caffeine.pot"
 
-touch $pot
-xgettext -j *.js -o $pot
-xgettext -j schemas/*.xml -o $pot
+touch "$pot"
+xgettext -j ./*.js -o "$pot"
+xgettext -j schemas/*.xml -o "$pot"
 
 for locale_lang in locale/*; do
-    po=$locale_lang/LC_MESSAGES/gnome-shell-extension-caffeine.po
-    echo $po
-    msgmerge --backup=off -U $po $pot
-    msgfmt $po -o ${po%po}mo
+    po="$locale_lang/LC_MESSAGES/gnome-shell-extension-caffeine.po"
+    echo "$po"
+    msgmerge --backup=off -U "$po" "$pot"
+    msgfmt "$po" -o "${po%po}mo"
 done
 
-rm $pot
+rm "$pot"
