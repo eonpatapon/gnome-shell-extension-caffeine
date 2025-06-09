0cecf128e73d87272bb808a7fe55b347a74766da
ochi12 <kaurichannel@gmail.com>
2025-06-09 21:25:04 +0800

    Initial Commit
     - still a lot of clean up to do
    
     - implement todos

--- caffeine@patapon.info/preferences/appsPage.js
+++ caffeine@patapon.info/preferences/appsPage.js
@@ -164,10 +164,10 @@ class CaffeineAppsPage extends Adw.PreferencesPage {
     }
 
     _onAddApp() {
-        const dialog = new NewAppDialog(this.get_root(), this._settings, this._settingsKey);
+        const dialog = new CustomAppChooser(this.get_root());
         dialog.connect('response', (dlg, id) => {
-            const appInfo = id === Gtk.ResponseType.OK
-                ? dialog.get_widget().get_app_info() : null;
+            const appInfo = id === CustomAppChooser.ResponseType.OK
+                ? dlg.appInfo : null;
             const apps = this._settings.get_strv(this._settingsKey.INHIBIT_APPS);
             if (appInfo && !apps.some((a) => a === appInfo.get_id())) {
                 this._settings.set_strv(this._settingsKey.INHIBIT_APPS, [
@@ -190,31 +190,214 @@ class CaffeineAppsPage extends Adw.PreferencesPage {
     }
 });
 
-const NewAppDialog = GObject.registerClass(
-    class NewAppDialog extends Gtk.AppChooserDialog {
-        _init(parent, settings, settingsKey) {
-            super._init({
-                transient_for: parent,
-                modal: true
-            });
-
-            this._settings = settings;
-            this._settingsKey = settingsKey;
-
-            this.get_widget().set({
-                show_all: true,
-                show_other: true // hide more button
-            });
-
-            this.get_widget().connect('application-selected',
-                this._updateSensitivity.bind(this));
-            this._updateSensitivity();
+const CustomAppChooser = GObject.registerClass({
+    Signals: {
+        'response': {
+            param_types: [GObject.TYPE_INT]
         }
+    }
+}, class CustomAppChooser extends Adw.Window {
+    _init(parent) {
+        super._init({
+            transient_for: parent,
+            modal: true
+        });
 
-        _updateSensitivity() {
-            const apps = this._settings.get_strv(this._settingsKey.INHIBIT_APPS);
-            const appInfo = this.get_widget().get_app_info();
-            this.set_response_sensitive(Gtk.ResponseType.OK,
-                appInfo && !apps.some((i) => i.startsWith(appInfo.get_id())));
-        }
-    });
+        this.set_size_request(500, 250);
+
+        this._appInfo = null;
+
+        const container = new Gtk.Box({
+            orientation: Gtk.Orientation.VERTICAL,
+            spacing: 0,
+            homogeneous: false
+        });
+
+        // Header headerBar
+        const headerBar = new Gtk.HeaderBar({
+            margin_bottom: 0
+        });
+        const cancelButton = Gtk.Button.new_with_label('Cancel');
+        const selectButton = Gtk.Button.new_with_label('Select');
+        const searchToggle = new Gtk.ToggleButton();
+        const toggleIcon = Gtk.Image.new_from_icon_name('edit-find-symbolic');
+        const titleWidget = new Gtk.Label({
+            label: 'Select Application'
+        });
+
+        cancelButton.add_css_class('raised');
+        selectButton.add_css_class('suggested-action');
+        searchToggle.add_css_class('flat');
+        headerBar.add_css_class('flat');
+        searchToggle.set_child(toggleIcon);
+        headerBar.set_show_title_buttons(false);
+        headerBar.pack_start(cancelButton);
+        headerBar.pack_end(selectButton);
+        headerBar.pack_end(searchToggle);
+        headerBar.set_title_widget(titleWidget);
+
+        // Revealer
+        const revealer = new Gtk.Revealer();
+        const entry = new Gtk.SearchEntry({
+            margin_start: 6,
+            margin_end: 6,
+            margin_bottom: 6
+        });
+        entry.add_css_class('search');
+
+        revealer.set_child(entry);
+
+        // AppList And Filters
+        const appInfosStore = this._getAppInfosStore();
+        const appFilter = Gtk.CustomFilter.new((appInfo) => {
+            const query = entry.text.toLowerCase();
+            return appInfo.get_display_name().toLowerCase().includes(query);
+        });
+
+        const filterModel = new Gtk.FilterListModel({
+            model: appInfosStore,
+            filter: appFilter
+        });
+
+        const singleSelection = Gtk.SingleSelection.new(filterModel);
+        const factory = new Gtk.SignalListItemFactory();
+        const listView = new Gtk.ListView({
+            model: singleSelection,
+            factory,
+            vexpand: true
+        });
+
+        const scrollView = new Gtk.ScrolledWindow({
+            vexpand: true,
+            margin_top: 0,
+            margin_bottom: 0,
+            margin_start: 6,
+            margin_end: 6,
+            min_content_height: 200,
+            max_content_height: 500,
+            has_frame: true
+        });
+        scrollView.set_child(listView);
+
+
+        // Bottom Bar
+        const findNewAppsButton = new Gtk.Button({
+            label: 'Find New Apps',
+            hexpand: true,
+            margin_top: 6,
+            margin_bottom: 6,
+            margin_start: 6,
+            margin_end: 6
+        });
+        findNewAppsButton.add_css_class('raised');
+
+        // Signals
+        searchToggle.connect('toggled', (obj) => {
+            if (obj.active) {
+                revealer.reveal_child = true;
+            } else {
+                revealer.reveal_child = false;
+            }
+        });
+
+        cancelButton.connect('clicked', () => {
+            this._appInfo = null;
+            this.emit('response', CustomAppChooser.ResponseType.CANCEL);
+            this.destroy();
+        });
+        selectButton.connect('clicked', () => {
+            const selectedItem = singleSelection.get_selected_item();
+            if (selectedItem) {
+                this._appInfo = selectedItem;
+                this.emit('response', CustomAppChooser.ResponseType.OK);
+            } else {
+                this._appInfo = null;
+                this.emit('response', CustomAppChooser.ResponseType.BAD);
+            }
+            this.destroy();
+        });
+
+        factory.connect('setup', (_factory, listItem) => {
+            const box = new Gtk.Box({ spacing: 6 });
+            const image = new Gtk.Image({ pixel_size: 24 });
+            const label = new Gtk.Label({ xalign: 0, hexpand: true });
+            box.append(image);
+            box.append(label);
+            listItem.set_child(box);
+        });
+        factory.connect('bind', (_factory, listItem) => {
+            const appInfo = listItem.get_item();
+            const icon = appInfo.get_icon();
+            const box = listItem.get_child();
+
+            const image = box.get_first_child();
+            const label = box.get_last_child();
+
+            if (icon instanceof Gio.ThemedIcon) {
+                image.set_from_gicon(icon);
+            } else if (icon instanceof Gio.FileIcon) {
+                const file = icon.get_file?.();
+                if (file) {
+                    const path = file.get_path();
+                    console.log(path);
+                    if (path) {
+                        image.set_from_file(path);
+                    }
+                }
+            }
+            label.set_label(appInfo.get_display_name());
+        });
+
+        singleSelection.connect('notify::selected', () => {
+            console.log('notifiy selected: singleSelection');
+        });
+
+        entry.connect('search-changed', (obj) => {
+            console.log(obj.get_text());
+            appFilter.changed(Gtk.FilterChange.DIFFERENT);
+        });
+
+        container.append(headerBar);
+        container.append(revealer);
+        container.append(scrollView);
+        container.append(findNewAppsButton);
+        this.set_content(container);
+    }
+
+    get appInfo() {
+        return this._appInfo;
+    }
+
+    _setupHeaderBar() {
+        // todo
+    }
+
+    _setupBottomBar() {
+        // todo
+    }
+
+    _setupScrolledWindow() {
+        // todo
+    }
+
+    _handleItemDoubleClick() {
+        // todo
+    }
+
+    _getAppInfosStore() {
+        const appInfos = Gio.AppInfo.get_all().filter((_appInfo) => _appInfo.should_show());
+        const store = new Gio.ListStore({ item_type: Gio.AppInfo });
+        appInfos.forEach((app) => {
+            store.append(app);
+            console.log(app.get_name());
+        });
+
+        return store;
+    }
+});
+
+CustomAppChooser.ResponseType = Object.freeze({
+    OK: 0,
+    CANCEL: 1,
+    BAD: 2
+});
